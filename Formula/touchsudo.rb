dbf3cec66aaefbea2293f3fceae6072a6650a56e
kashyap07 <kashyapsuhas07@gmail.com>
2026-01-20 03:27:09 +0530

    add SHA256 for v1.0.0

--- Formula/touchsudo.rb
+++ Formula/touchsudo.rb
@@ -2,7 +2,7 @@ class Touchsudo < Formula
   desc "Enable Touch ID for sudo authentication on macOS"
   homepage "https://github.com/kashyap07/homebrew-touchsudo"
   url "https://github.com/kashyap07/homebrew-touchsudo/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "REPLACE_AFTER_RELEASE"
+  sha256 "05365cbf429fdbf78879f9377129181cbe9c9e8e46854c918972d1ba1494d239"
   license "MIT"
   head "https://github.com/kashyap07/homebrew-touchsudo.git", branch: "main"
 
